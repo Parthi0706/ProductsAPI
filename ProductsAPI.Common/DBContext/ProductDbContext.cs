7d272344e2231450e211581610a9ac2f83b436f9
Parthiban R <parthi98.r@gmail.com>
2024-12-24 09:15:34 +0530

    Added Appsetting details and Dbcontext

---
+++ ProductsAPI.Common/DBContext/ProductDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Models.Products;
+
+namespace ProductsAPI.Common.DBContext
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) { }
+
+        public DbSet<Products> Products { get; set; }
+    }
+}

5fa2611b8c145c2c7235a6afb37fad5e47bb6454
Parthiban R <parthi98.r@gmail.com>
2024-12-24 09:25:18 +0530

    Add log4net for logg and added dependency injection to call the DB method from controller

--- ProductsAPI/Program.cs
+++ ProductsAPI/Program.cs
@@ -1,14 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Common.DBContext;
+using ProductsAPI.DataAccess.Contract;
+using ProductsAPI.DataAccess.Repository;
 
+var builder = WebApplication.CreateBuilder(args);
+#region for logging
+builder.Logging.ClearProviders();
+builder.Logging.AddLog4Net();
+#endregion
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<ProductDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductsDb")));
 var app = builder.Build();
-
+builder.Services.AddScoped<IProductDBManager, ProductDBManager>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {

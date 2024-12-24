b796570ba2ae8faa5ad0c0ae805d9dd3d2ed9f2b
Parthiban R <parthi98.r@gmail.com>
2024-12-24 09:35:29 +0530

    Added custom filter and validation message

---
+++ ProductsAPI.Common/Common/Customfilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsAPI.Common.Common
+{
+    public class RequireQueryParameterAttribute : ActionFilterAttribute
+    {
+        private readonly string _parameterName;
+
+        public RequireQueryParameterAttribute(string parameterName)
+        {
+            _parameterName = parameterName;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var hasParameter = context.HttpContext.Request.Query.ContainsKey(_parameterName);
+
+            if (!hasParameter)
+            {
+                context.Result = new BadRequestObjectResult($"The '{_parameterName}' query parameter is required.");
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
---
+++ ProductsAPI.Common/Common/Validationmessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsAPI.Common.Common
+{
+    public static class SaveUpdateMessage
+    {
+        public const string Saved = "Save Success";
+        public const string Update = "Updated";
+        public const string Deleted = "Delete Success";
+        public const string Error = "Something went Wrong Please try after sometime";
+        public const string Busy = "System is Busy try After sometime";
+        public const string NoRecordsInsert = "No records is there to Insert";
+        public const string NoRecordsUpdate = "No records is there to Update";
+    }
+}

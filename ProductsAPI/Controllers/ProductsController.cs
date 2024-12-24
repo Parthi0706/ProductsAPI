de3911526c405444438724eef6b92b9b37d4d96f
Parthiban R <parthi98.r@gmail.com>
2024-12-24 08:03:56 +0530

    ProductsAPI Skeleten created to work

---
+++ ProductsAPI/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
